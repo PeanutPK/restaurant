0d40026131050da2278a884bb604d5ae07757a28
Phiranath PO-NGERN <phukaow18@gmail.com>
2025-10-15 19:56:35 +0700

    add in class part exercise

---
+++ src/main/java/ku/cs/restaurant/controller/RestaurantController.java
@@ -0,0 +1,32 @@
+package ku.cs.restaurant.controller;
+
+import ku.cs.restaurant.entity.Restaurant;
+import ku.cs.restaurant.service.RestaurantService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class RestaurantController {
+
+    private RestaurantService service;
+
+    @Autowired
+    public RestaurantController(RestaurantService service) {
+        this.service = service;
+    }
+
+    @GetMapping("/restaurant")
+    public List<Restaurant> getAllRestaurants() {
+        return service.getAll();
+    }
+
+    @PostMapping("/restaurant")
+    public Restaurant save(@RequestBody Restaurant restaurant) {
+        return service.create(restaurant);
+    }
+}
---
+++ src/main/java/ku/cs/restaurant/service/RestaurantService.java
@@ -0,0 +1,28 @@
+package ku.cs.restaurant.service;
+
+import ku.cs.restaurant.entity.Restaurant;
+import ku.cs.restaurant.repository.RestaurantRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.util.List;
+
+@Service
+public class RestaurantService {
+    private final RestaurantRepository repository;
+
+    @Autowired
+    public RestaurantService(RestaurantRepository repository) {
+        this.repository = repository;
+    }
+
+    public List<Restaurant> getAll() {
+        return repository.findAll();
+    }
+
+    public Restaurant create(Restaurant restaurant) {
+        restaurant.setCreatedAt(Instant.now());
+        return repository.save(restaurant);
+    }
+}
